431ed92ef28657a1864be9e104567fccd4e38e61
suhani93 <suhani93@naver.com>
2021-10-11 19:59:47 +0900

    #8 - [추가] | 개발환경 세팅

---
+++ src/test/java/product/demo/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package product.demo.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
