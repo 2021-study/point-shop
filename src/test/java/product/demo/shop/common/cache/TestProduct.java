491d4b792b33845fd721b69100c4c8f3f7af4fac
humanitas03 <dlswp113@gmail.com>
2021-10-16 20:35:56 +0900

    ThreadLocalRandom으로 수정

--- src/test/java/product/demo/shop/common/cache/TestProduct.java
+++ src/test/java/product/demo/shop/common/cache/TestProduct.java
@@ -1,7 +1,7 @@
 package product.demo.shop.common.cache;
 
 import java.math.BigDecimal;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -16,6 +16,6 @@ public class TestProduct {
     private BigDecimal price;
 
     public static TestProduct sampleData() {
-        return new TestProduct(new Random().nextLong()/100L, "001", "000001", "0000001", BigDecimal.TEN);
+        return new TestProduct(ThreadLocalRandom.current().nextLong(10000000), "001", "000001", "0000001", BigDecimal.TEN);
     }
 }
