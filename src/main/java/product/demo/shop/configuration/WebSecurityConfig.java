bf3551a7aae497c947f801f186b2f181a6e28922
suhani93 <suhani93@naver.com>
2021-10-31 19:57:44 +0900

    PasswordEncoder 적용 및 UserDetailsService 세팅

--- src/main/java/product/demo/shop/configuration/WebSecurityConfig.java
+++ src/main/java/product/demo/shop/configuration/WebSecurityConfig.java
@@ -1,12 +1,30 @@
 package product.demo.shop.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import product.demo.shop.domain.user.service.CustomUserDetailsService;
+
+import javax.sql.DataSource;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .userDetailsService(customUserDetailsService)
+            .passwordEncoder(passwordEncoder);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
---
+++ src/main/java/product/demo/shop/domain/user/service/CustomUserDetailsService.java
@@ -0,0 +1,18 @@
+package product.demo.shop.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import product.demo.shop.domain.user.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        //TODO 유저 로그인을 위해 UserDetails를 상속받은 클래스를 만든 후 로직에 맞게 구현 필요합니다.
+        return null;
+    }
+}
