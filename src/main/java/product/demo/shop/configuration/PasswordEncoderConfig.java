6f1388cdf247e65b92d4a80ff2129b3935879241
suhani93 <suhani93@naver.com>
2021-10-31 17:26:49 +0900

    password 인코더 멀티 encoder 세팅

--- src/main/java/product/demo/shop/configuration/PasswordEncoderConfig.java
+++ src/main/java/product/demo/shop/configuration/PasswordEncoderConfig.java
@@ -3,12 +3,23 @@ package product.demo.shop.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class PasswordEncoderConfig {
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    public PasswordEncoder delegatingPasswordEncoder() {
+        Map<String, PasswordEncoder> encoders = new HashMap();
+        String defaultPasswordAlgorithm = "bcrypt";
+        BCryptPasswordEncoder defaultPasswordEncoder = new BCryptPasswordEncoder();
+        encoders.put(defaultPasswordAlgorithm, defaultPasswordEncoder);
+
+        DelegatingPasswordEncoder delegatingPasswordEncoder = new DelegatingPasswordEncoder(defaultPasswordAlgorithm, encoders);
+        delegatingPasswordEncoder.setDefaultPasswordEncoderForMatches(defaultPasswordEncoder);
+
+        return delegatingPasswordEncoder;
     }
 }
--- src/test/java/product/demo/shop/configuration/PasswordEncoderConfigTest.java
+++ src/test/java/product/demo/shop/configuration/PasswordEncoderConfigTest.java
@@ -1,9 +1,11 @@
 package product.demo.shop.configuration;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,21 +17,39 @@ class PasswordEncoderConfigTest {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @Test
-    void 비밀번호_암호화_테스트(){
-        String planPassword = "1234asdf!@#$";
-        String encodedPassword = passwordEncoder.encode(planPassword);
+    @Nested
+    class 비밀번호_암호화_테스트{
+        @Test
+        void 디폴트_bcrypt_암호화_테스트(){
+            String planPassword = "1234asdf!@#$";
+            String encodedPassword = passwordEncoder.encode(planPassword);
 
-        assertThat(planPassword, not(equalTo(encodedPassword)));
-        Assertions.assertDoesNotThrow(() -> {
-            passwordEncoder.matches(planPassword,encodedPassword);
-        });
+
+            assertThat(planPassword, not(equalTo(encodedPassword)));
+            assertThat(encodedPassword, startsWith("{bcrypt}"));
+            Assertions.assertDoesNotThrow(() -> {
+                passwordEncoder.matches(planPassword,encodedPassword);
+            });
+        }
     }
 
-    @Test
-    void 비밀번호_매칭_테스트(){
-        String encodedPassword = "$2a$10$XSRY968YOaQ0MQUYezoqQ.tes.1hn9AOhHeq3VXw9EB4.GHefq6Ee";
-        String planPassword = "1234asdf!@#$";
-        assertThat(passwordEncoder.matches(planPassword,encodedPassword), is(equalTo(true)));
+
+    @Nested
+    class 비밀번호_매칭_테스트{
+        @Test
+        void bcrypt로_암호화된_비밀번호_매칭_테스트(){
+            String encodedPassword = "{bcrypt}$2a$10$XSRY968YOaQ0MQUYezoqQ.tes.1hn9AOhHeq3VXw9EB4.GHefq6Ee";
+            String planPassword = "1234asdf!@#$";
+            assertThat(passwordEncoder.matches(planPassword,encodedPassword), is(equalTo(true)));
+        }
+
+        @Test
+        void 암호화_알고리즘_prefix가_없는_비밀번호는_bcrypt로_매칭되는지_테스트(){
+            String encodedPassword = "$2a$10$XSRY968YOaQ0MQUYezoqQ.tes.1hn9AOhHeq3VXw9EB4.GHefq6Ee";
+            String planPassword = "1234asdf!@#$";
+            assertThat(passwordEncoder.matches(planPassword,encodedPassword), is(equalTo(true)));
+        }
     }
+
+
 }
