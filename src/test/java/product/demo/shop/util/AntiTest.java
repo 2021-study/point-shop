8696dee616f338dfbc13e0e8b657db690d6abb4f
humanitas03 <dlswp113@gmail.com>
2021-11-04 21:51:29 +0900

    Anti test

---
+++ src/test/java/product/demo/shop/util/AntiTest.java
@@ -0,0 +1,13 @@
+package product.demo.shop.util;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AntiTest {
+
+    @Test
+    public void antiTest() {
+        assertTrue(false);
+    }
+}
